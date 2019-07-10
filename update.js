f06d5cd086e64edea9c34ca762c7abf58a13f8e0
Ari MB <arimb1999@gmail.com>
2019-07-10 15:10:53 +0300

    first upload

---
+++ update.js
@@ -0,0 +1,157 @@
+var raw_teams = `$195	Ankit	yote	$66 FRC 1114 Simbotics, $76 FRC 2056 OP Robotics, $53 FRC 2910 Jack in the Bot
+$198	Braxton	Hype Squad	$66 FRC 1114 Simbotics, $76 FRC 2056 OP Robotics, $19 FRC 2403 Plasma Robotics, $37 FRC 2468 Team Appreciate
+$193	Josiah	The Super Team	$22 FRC 111 WildStang, $35 FRC 1241 Theory 6, $27 FRC 1684 The Chimeras, $38 FRC 2481 Roboteers, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs
+$198	Wyatt		$30 FRC 118 Robonauts, $49 FRC 330 The Beach Bots, $66 FRC 1114 Simbotics, $53 FRC 2910 Jack in the Bot
+$196	Corbin	FIMstein	$39 FRC 33 Killer Bees, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs, $40 FRC 4362 The Gems, $46 FRC 5460 Strike Zone
+$200	Anthony	Just FiM	$22 FRC 111 WildStang, $30 FRC 118 Robonauts, $25 FRC 548 Robostangs, $11 FRC 1024 Kil-A-Bytes, $19 FRC 1923 The MidKnight Inventors, $20 FRC 3357 The COMETS, $19 FRC 3604 Goon Squad, $34 FRC 3707 Technodogs, $20 FRC 4607 C.I.S.
+$197	Joel		$20 FRC 340 Greater Rochester Robotics, $66 FRC 1114 Simbotics, $35 FRC 1241 Theory 6, $76 FRC 2056 OP Robotics
+$200	Chunche	Cheesecake.Mecanum.Swerve	$47 FRC 195 CyberKnights, $25 FRC 548 Robostangs, $36 FRC 910 The Foley Freeze, $36 FRC 930 Mukwonago BEARs, $18 FRC 4028 the Beak Squad, $38 FRC 5406 Celt-X
+$200	Gerald	$exyBotys	$13 FRC 48 Team E.L.I.T.E., $30 FRC 118 Robonauts, $47 FRC 195 CyberKnights, $35 FRC 1241 Theory 6, $40 FRC 2168 The Aluminum Falcons, $35 FRC 3847 Spectrum
+$197	Nik		$39 FRC 33 Killer Bees, $47 FRC 195 CyberKnights, $29 FRC 2075 Enigma Robotics, $25 FRC 2337 EngiNERDs, $37 FRC 2767 Stryke Force, $20 FRC 3357 The COMETS
+$191	Rahul	Optional: Fantasy Team Name	$47 FRC 195 CyberKnights, $66 FRC 1114 Simbotics, $40 FRC 2168 The Aluminum Falcons, $38 FRC 5406 Celt-X
+$200	Connor	JSM	$28 FRC 364 Team Fusion, $15 FRC 1073 The Force Team, $35 FRC 1241 Theory 6, $13 FRC 1410 The Kraken, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs, $38 FRC 5406 Celt-X
+$199	Nikith	stang gang fan club	$22 FRC 111 WildStang, $25 FRC 548 Robostangs, $15 FRC 1073 The Force Team, $66 FRC 1114 Simbotics, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs
+$200	Dan	RPs are overrated 	$30 FRC 118 Robonauts, $35 FRC 1241 Theory 6, $16 FRC 1720 PhyXTGears, $25 FRC 2337 EngiNERDs, $37 FRC 2767 Stryke Force, $20 FRC 3357 The COMETS, $19 FRC 3604 Goon Squad, $18 FRC 4028 the Beak Squad
+$200	Stuart	Purple Spam	$39 FRC 33 Killer Bees, $22 FRC 111 WildStang, $28 FRC 364 Team Fusion, $25 FRC 548 Robostangs, $15 FRC 1073 The Force Team, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs
+$199	Ryan	Ginger Power	$39 FRC 33 Killer Bees, $22 FRC 111 WildStang, $30 FRC 118 Robonauts, $35 FRC 1241 Theory 6, $35 FRC 3847 Spectrum, $38 FRC 5406 Celt-X
+$199	Garrett		$28 FRC 364 Team Fusion, $17 FRC 461 Westside Boiler Invasion, $24 FRC 868 Techhounds, $15 FRC 1073 The Force Team, $27 FRC 1684 The Chimeras, $16 FRC 1720 PhyXTGears, $25 FRC 2337 EngiNERDs, $20 FRC 3357 The COMETS, $14 FRC 3940 CyberTooth, $13 FRC 5205 Full Metal Jackets
+$197	James	Riot Crew Fantasy Two	$39 FRC 33 Killer Bees, $30 FRC 118 Robonauts, $47 FRC 195 CyberKnights, $34 FRC 225 TechFire, $28 FRC 319 Big Bad BOB, $19 FRC 1923 The MidKnight Inventors
+$200	Alum	citrus circus	$36 FRC 217 ThunderChickens, $37 FRC 2767 Stryke Force, $55 FRC 3538 RoboJackets, $34 FRC 3707 Technodogs, $38 FRC 5406 Celt-X
+$199	Ben	Popped Cargo #2	$39 FRC 33 Killer Bees, $36 FRC 217 ThunderChickens, $15 FRC 1073 The Force Team, $38 FRC 2481 Roboteers, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs
+$194	Evan	Brian’s Griffin	$13 FRC 48 Team E.L.I.T.E., $22 FRC 111 WildStang, $25 FRC 548 Robostangs, $36 FRC 930 Mukwonago BEARs, $24 FRC 1023 Bedford Express, $34 FRC 3707 Technodogs, $20 FRC 4607 C.I.S., $20 FRC 7498 Wingus & Dingus
+$200	Ruoyang	ns_is_watching_u	$47 FRC 195 CyberKnights, $36 FRC 217 ThunderChickens, $28 FRC 319 Big Bad BOB, $55 FRC 3538 RoboJackets, $34 FRC 3707 Technodogs
+$193	Henry		$15 FRC 1073 The Force Team, $35 FRC 1241 Theory 6, $19 FRC 2403 Plasma Robotics, $53 FRC 2910 Jack in the Bot, $11 FRC 3478 LamBot, $22 FRC 6443 AEMBOT, $18 FRC 7457 suPURDUEper Robotics, $20 FRC 7498 Wingus & Dingus
+$195	Louis	lc910’s fantasy picks	$36 FRC 910 The Foley Freeze, $15 FRC 1073 The Force Team, $37 FRC 2767 Stryke Force, $19 FRC 3604 Goon Squad, $34 FRC 3707 Technodogs, $32 FRC 5190 Green Hope Robotics, $22 FRC 6443 AEMBOT
+$200	Amy	Second School	$20 FRC 340 Greater Rochester Robotics, $66 FRC 1114 Simbotics, $76 FRC 2056 OP Robotics, $38 FRC 5406 Celt-X
+$200	Milly	EnterContender	$76 FRC 2056 OP Robotics, $37 FRC 2767 Stryke Force, $53 FRC 2910 Jack in the Bot, $34 FRC 3707 Technodogs
+$200	Eric	Quantity =ish Quality	$13 FRC 48 Team E.L.I.T.E., $30 FRC 118 Robonauts, $25 FRC 548 Robostangs, $24 FRC 868 Techhounds, $11 FRC 1024 Kil-A-Bytes, $13 FRC 1410 The Kraken, $10 FRC 1730 Team Driven, $25 FRC 2337 EngiNERDs, $11 FRC 3478 LamBot, $18 FRC 7457 suPURDUEper Robotics, $20 FRC 7498 Wingus & Dingus
+$196	Reed 	Finally	$30 FRC 118 Robonauts, $37 FRC 2468 Team Appreciate, $37 FRC 2767 Stryke Force, $19 FRC 3604 Goon Squad, $35 FRC 3847 Spectrum, $38 FRC 5406 Celt-X
+$198	Kaleb	TeamPlayoffs	$15 FRC 1073 The Force Team, $66 FRC 1114 Simbotics, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs, $46 FRC 5460 Strike Zone
+$200	Jacob	FULL SEND 	$39 FRC 33 Killer Bees, $49 FRC 330 The Beach Bots, $66 FRC 1114 Simbotics, $11 FRC 3478 LamBot, $35 FRC 3847 Spectrum
+$200	Colin	multi-climb masters	$15 FRC 1073 The Force Team, $19 FRC 1923 The MidKnight Inventors, $37 FRC 2767 Stryke Force, $53 FRC 2910 Jack in the Bot, $34 FRC 3707 Technodogs, $22 FRC 6443 AEMBOT, $20 FRC 7498 Wingus & Dingus
+$194	Jeff	Chaotic Neutral	$39 FRC 33 Killer Bees, $22 FRC 111 WildStang, $30 FRC 118 Robonauts, $47 FRC 195 CyberKnights, $36 FRC 217 ThunderChickens, $20 FRC 7498 Wingus & Dingus
+$200	Noud	Superscout-Noud	$13 FRC 48 Team E.L.I.T.E., $22 FRC 111 WildStang, $18 FRC 234 Cyber Blue, $17 FRC 461 Westside Boiler Invasion, $24 FRC 1023 Bedford Express, $11 FRC 1024 Kil-A-Bytes, $10 FRC 1730 Team Driven, $11 FRC 3478 LamBot, $14 FRC 3940 CyberTooth, $18 FRC 4028 the Beak Squad, $13 FRC 5205 Full Metal Jackets, $14 FRC 5511 Cortechs Robotics, $15 FRC 5801 CTC Inspire
+$195	jonatahn		$66 FRC 1114 Simbotics, $76 FRC 2056 OP Robotics, $53 FRC 2910 Jack in the Bot
+$200	Nathan	NoBlueBanner-NBB	$47 FRC 195 CyberKnights, $66 FRC 1114 Simbotics, $25 FRC 2337 EngiNERDs, $28 FRC 3641 The Flying Toasters, $34 FRC 3707 Technodogs
+$199	Ishaan		$39 FRC 33 Killer Bees, $36 FRC 217 ThunderChickens, $49 FRC 330 The Beach Bots, $38 FRC 2481 Roboteers, $37 FRC 2767 Stryke Force
+$200	Curtis	Better than Nothing	$18 FRC 51 The Wings of Fire, $18 FRC 234 Cyber Blue, $28 FRC 319 Big Bad BOB, $28 FRC 364 Team Fusion, $25 FRC 548 Robostangs, $15 FRC 1073 The Force Team, $17 FRC 1676 The Pascack Pi-oneers, $22 FRC 1718 The Fighting Pi, $10 FRC 1730 Team Driven, $19 FRC 1923 The MidKnight Inventors
+$199	Travis	Cheap & Ugly	$13 FRC 48 Team E.L.I.T.E., $18 FRC 234 Cyber Blue, $15 FRC 1073 The Force Team, $13 FRC 1410 The Kraken, $16 FRC 1720 PhyXTGears, $10 FRC 1730 Team Driven, $19 FRC 1923 The MidKnight Inventors, $11 FRC 3478 LamBot, $14 FRC 3940 CyberTooth, $13 FRC 5205 Full Metal Jackets, $15 FRC 5801 CTC Inspire, $22 FRC 6443 AEMBOT, $20 FRC 7498 Wingus & Dingus
+$200	Landon		$30 FRC 118 Robonauts, $28 FRC 364 Team Fusion, $66 FRC 1114 Simbotics, $76 FRC 2056 OP Robotics
+$200	Kevin		$30 FRC 118 Robonauts, $25 FRC 548 Robostangs, $36 FRC 910 The Foley Freeze, $38 FRC 2481 Roboteers, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs
+$200	Nathan		$28 FRC 319 Big Bad BOB, $15 FRC 1073 The Force Team, $66 FRC 1114 Simbotics, $76 FRC 2056 OP Robotics, $15 FRC 5801 CTC Inspire
+$193	Tyler	FUN Times at IRI High	$28 FRC 364 Team Fusion, $35 FRC 1241 Theory 6, $19 FRC 1923 The MidKnight Inventors, $37 FRC 2767 Stryke Force, $20 FRC 3357 The COMETS, $34 FRC 3707 Technodogs, $20 FRC 7498 Wingus & Dingus
+$199	Lucas		$39 FRC 33 Killer Bees, $22 FRC 111 WildStang, $47 FRC 195 CyberKnights, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs, $20 FRC 4776 S.C.O.T.S. Bots
+$199	Matt	The Dragons 	$22 FRC 111 WildStang, $25 FRC 548 Robostangs, $15 FRC 1073 The Force Team, $37 FRC 2767 Stryke Force, $11 FRC 3478 LamBot, $55 FRC 3538 RoboJackets, $34 FRC 3707 Technodogs
+$199	Utkarsh 	Business 	$47 FRC 195 CyberKnights, $49 FRC 330 The Beach Bots, $16 FRC 1720 PhyXTGears, $53 FRC 2910 Jack in the Bot, $34 FRC 3707 Technodogs
+$198	Perry	Optional: Fantasy Team Name	$15 FRC 1073 The Force Team, $66 FRC 1114 Simbotics, $10 FRC 1730 Team Driven, $53 FRC 2910 Jack in the Bot, $20 FRC 3357 The COMETS, $34 FRC 3707 Technodogs
+$199	Preston	Official Pchild Ratings	$22 FRC 111 WildStang, $28 FRC 319 Big Bad BOB, $25 FRC 548 Robostangs, $15 FRC 1073 The Force Team, $35 FRC 1241 Theory 6, $34 FRC 3707 Technodogs, $18 FRC 4028 the Beak Squad, $22 FRC 6443 AEMBOT
+$196	Tug		$47 FRC 195 CyberKnights, $36 FRC 217 ThunderChickens, $76 FRC 2056 OP Robotics, $37 FRC 2767 Stryke Force
+$200	Zack	Toad Torque	$39 FRC 33 Killer Bees, $30 FRC 118 Robonauts, $49 FRC 330 The Beach Bots, $20 FRC 340 Greater Rochester Robotics, $28 FRC 3641 The Flying Toasters, $34 FRC 3707 Technodogs
+$200	Conall		$15 FRC 1073 The Force Team, $19 FRC 1747 Harrison Boiler Robotics, $19 FRC 1923 The MidKnight Inventors, $38 FRC 2481 Roboteers, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs, $38 FRC 5406 Celt-X
+$199	Jennifer	Space Jackets	$39 FRC 33 Killer Bees, $30 FRC 118 Robonauts, $36 FRC 217 ThunderChickens, $37 FRC 2468 Team Appreciate, $35 FRC 3847 Spectrum, $22 FRC 6443 AEMBOT
+$199	Pete		$13 FRC 48 Team E.L.I.T.E., $22 FRC 111 WildStang, $24 FRC 1023 Bedford Express, $11 FRC 1024 Kil-A-Bytes, $13 FRC 1410 The Kraken, $22 FRC 1718 The Fighting Pi, $25 FRC 2337 EngiNERDs, $38 FRC 2481 Roboteers, $11 FRC 3478 LamBot, $20 FRC 4607 C.I.S.
+$193	Theo	full send	$39 FRC 33 Killer Bees, $47 FRC 195 CyberKnights, $36 FRC 217 ThunderChickens, $49 FRC 330 The Beach Bots, $22 FRC 6443 AEMBOT
+$200	Jason		$22 FRC 111 WildStang, $47 FRC 195 CyberKnights, $36 FRC 217 ThunderChickens, $24 FRC 1023 Bedford Express, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs
+$197	Sophia		$39 FRC 33 Killer Bees, $30 FRC 118 Robonauts, $49 FRC 330 The Beach Bots, $66 FRC 1114 Simbotics, $13 FRC 5205 Full Metal Jackets
+$199	Garrison		$28 FRC 364 Team Fusion, $13 FRC 1410 The Kraken, $16 FRC 1720 PhyXTGears, $10 FRC 1730 Team Driven, $37 FRC 2468 Team Appreciate, $37 FRC 2767 Stryke Force, $20 FRC 3357 The COMETS, $18 FRC 4028 the Beak Squad, $20 FRC 4776 S.C.O.T.S. Bots
+$238	Sean	Team Propel	$13 FRC 48 Team E.L.I.T.E., $22 FRC 111 WildStang, $30 FRC 118 Robonauts, $47 FRC 195 CyberKnights, $36 FRC 930 Mukwonago BEARs, $15 FRC 1073 The Force Team, $53 FRC 2910 Jack in the Bot, $22 FRC 6443 AEMBOT
+$199	Josh		$39 FRC 33 Killer Bees, $47 FRC 195 CyberKnights, $53 FRC 2910 Jack in the Bot, $38 FRC 5406 Celt-X, $22 FRC 6443 AEMBOT
+$200	Liam		$49 FRC 330 The Beach Bots, $76 FRC 2056 OP Robotics, $38 FRC 2481 Roboteers, $37 FRC 2767 Stryke Force
+$200	Lukas	WLN	$30 FRC 118 Robonauts, $36 FRC 217 ThunderChickens, $34 FRC 225 TechFire, $15 FRC 1073 The Force Team, $13 FRC 1410 The Kraken, $34 FRC 3707 Technodogs, $38 FRC 5406 Celt-X
+$200	Nathan	Dream Team	$30 FRC 118 Robonauts, $36 FRC 217 ThunderChickens, $15 FRC 1073 The Force Team, $17 FRC 1676 The Pascack Pi-oneers, $40 FRC 2168 The Aluminum Falcons, $28 FRC 3641 The Flying Toasters, $34 FRC 3707 Technodogs
+$226	Anthony	Stinky Pete and the Knees	$39 FRC 33 Killer Bees, $47 FRC 195 CyberKnights, $36 FRC 217 ThunderChickens, $34 FRC 225 TechFire, $35 FRC 1241 Theory 6, $35 FRC 3847 Spectrum
+$198	Brennan	Team Bubbles	$30 FRC 118 Robonauts, $28 FRC 319 Big Bad BOB, $24 FRC 1023 Bedford Express, $15 FRC 1073 The Force Team, $17 FRC 1676 The Pascack Pi-oneers, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs, $13 FRC 5205 Full Metal Jackets
+$200	Joel	The winner	$30 FRC 118 Robonauts, $25 FRC 548 Robostangs, $27 FRC 1684 The Chimeras, $22 FRC 1718 The Fighting Pi, $25 FRC 2337 EngiNERDs, $37 FRC 2767 Stryke Force, $34 FRC 3707 Technodogs
+$197	Heather	Popped Cargo2	$47 FRC 195 CyberKnights, $34 FRC 225 TechFire, $11 FRC 1024 Kil-A-Bytes, $17 FRC 1676 The Pascack Pi-oneers, $16 FRC 1720 PhyXTGears, $10 FRC 1730 Team Driven, $11 FRC 3478 LamBot, $19 FRC 3604 Goon Squad, $14 FRC 3940 CyberTooth, $18 FRC 7457 suPURDUEper Robotics
+$200	Ethan	CheesePuffs	$13 FRC 48 Team E.L.I.T.E., $22 FRC 111 WildStang, $28 FRC 319 Big Bad BOB, $37 FRC 2468 Team Appreciate, $38 FRC 2481 Roboteers, $28 FRC 3641 The Flying Toasters, $34 FRC 3707 Technodogs
+$195	Dominique		$39 FRC 33 Killer Bees, $30 FRC 118 Robonauts, $36 FRC 217 ThunderChickens, $28 FRC 319 Big Bad BOB, $28 FRC 1807 Redbird Robotics, $34 FRC 3707 Technodogs
+	Brett		$39 FRC 33 Killer Bees, $30 FRC 118 Robonauts, $36 FRC 930 Mukwonago BEARs, $37 FRC 2767 Stryke Force, $38 FRC 5406 Celt-X, $20 FRC 7498 Wingus & Dingus
+	Bernadette	Full Beans	$39 FRC 33 Killer Bees, $30 FRC 118 Robonauts, $66 FRC 1114 Simbotics, $27 FRC 1684 The Chimeras, $37 FRC 2767 Stryke Force
+	Josiah	MikLast	$18 FRC 234 Cyber Blue, $24 FRC 1023 Bedford Express, $15 FRC 1073 The Force Team, $13 FRC 1410 The Kraken, $10 FRC 1730 Team Driven, $28 FRC 1807 Redbird Robotics, $11 FRC 3478 LamBot, $34 FRC 3707 Technodogs, $46 FRC 5460 Strike Zone
+	Ari		$13 FRC 48 Team E.L.I.T.E., $22 FRC 111 WildStang, $30 FRC 118 Robonauts, $18 FRC 234 Cyber Blue, $28 FRC 319 Big Bad BOB, $24 FRC 1023 Bedford Express, $22 FRC 1718 The Fighting Pi, $10 FRC 1730 Team Driven, $19 FRC 1747 Harrison Boiler Robotics, $11 FRC 3478 LamBot`;
+
+var pickem = [];
+
+$(document).ready(function(){
+	$('button#go').click(update)
+	raw_teams.split('\n').forEach(function(row){
+		var elements = row.split('\t');
+		if(parseInt(elements[0].substring(2))>200) return;
+		var name = elements[2];
+		if(name==='') name = elements[1];
+		var picks = elements[3].split(', ').map(function(str){
+			var start = str.indexOf('FRC ')+4;
+			return str.substring(start, str.indexOf(' ', start));
+		});
+		pickem.push([name, picks]);
+		var row = $('table#pickem tbody')[0].insertRow(-1);
+		row.insertCell(0).innerHTML = name;
+		row.insertCell(1).innerHTML = picks.join(', ');
+		row.insertCell(2);
+		$('tbody td').css('text-align', 'left');
+	});
+	$('button#go').click();
+});
+
+function update(){
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://www.thebluealliance.com/api/v3/event/2019iri/teams/statuses', true);
+	request.setRequestHeader('X-TBA-Auth-Key', 'h28l9eYEBtOCXpcFQN821YZRbjr0rTh2UdGFwqVf2jb36Sjvx2xYyUrZB5MPVJwv');
+	request.setRequestHeader('accept', 'application/json');
+	request.onload = function(){
+		var data = JSON.parse(this.response);
+		var scores = pickem.map(val => val[1].map(pick => points(data['frc'+pick])).reduce((a,b) => a+b, 0));
+		pickem2 = zip([pickem, scores]).sort(x => x[1]);
+		$('table#pickem tbody').html('');
+		pickem2.forEach(function(val){
+			var row = $('table#pickem tbody')[0].insertRow(-1);
+			row.insertCell(0).innerHTML = val[0][0];
+			row.insertCell(1).innerHTML = val[0][1];
+			row.insertCell(2).innerHTML = val[1];
+		});
+		$('div#last-updated').html('Last updated ' + new Date().toLocaleTimeString());
+	};
+	request.send();
+}
+
+function points(status){
+	var tmp = Math.ceil(7.676*erfinv((status['qual']['num_teams']-2*status['qual']['ranking']['rank']+2)/(1.07*status['qual']['num_teams']))+12);
+	if(status['alliance']){
+		if(status['alliance']['pick']<=1) tmp += 17-status['alliance']['number'];
+		else if(status['alliance']['pick']==2) tmp += status['alliance']['number'];
+	}
+	if(status['playoff']) tmp += status['playoff']['record']['wins']*5;
+	return tmp;
+}
+
+function erfinv(x){
+        var z;
+        var a  = 0.147;                                                   
+        var the_sign_of_x;
+        if(0==x) {
+            the_sign_of_x = 0;
+        } else if(x>0){
+            the_sign_of_x = 1;
+        } else {
+            the_sign_of_x = -1;
+        }
+
+        if(0 != x) {
+            var ln_1minus_x_sqrd = Math.log(1-x*x);
+            var ln_1minusxx_by_a = ln_1minus_x_sqrd / a;
+            var ln_1minusxx_by_2 = ln_1minus_x_sqrd / 2;
+            var ln_etc_by2_plus2 = ln_1minusxx_by_2 + (2/(Math.PI * a));
+            var first_sqrt = Math.sqrt((ln_etc_by2_plus2*ln_etc_by2_plus2)-ln_1minusxx_by_a);
+            var second_sqrt = Math.sqrt(first_sqrt - ln_etc_by2_plus2);
+            z = second_sqrt * the_sign_of_x;
+        } else { // x is zero
+            z = 0;
+        }
+  return z;
+}
+
+function zip(arrays) {
+    return Array.apply(null,Array(arrays[0].length)).map(function(_,i){
+        return arrays.map(function(array){return array[i]})
+    });
+}
